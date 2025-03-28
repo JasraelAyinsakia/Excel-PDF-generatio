061d7e8e098f985f0e1268bfb82f5890fe667eee
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-03-28 14:42:28 +0000

    Add header

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    columns = list(df.columns)
-    pdf.set_font(family="Times", size=10)
+
+    #Add a header
+    columns = df.columns
+    columns = [item.replace("-", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style= "B")
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
     pdf.cell(w=70, h=8, txt=columns[1], border=1)
